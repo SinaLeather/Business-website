b1dbad97aa94bc229c4ff5a6aa0ea3bbc12e125b
maki mamo <marakiastateke930@gmail.com>
2024-10-11 10:57:12 +0300

    register form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,34 @@
+import React, { useState } from 'react';
 import Navbar from './components/NavBar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import RegistrationForm from './components/RegistrationForm';
 import HeroSecition from './components/hero';
 import CategorySection from './components/category.';
 import Card from './components/card';
 import Testimonial from './components/Testimonial';
 import AddOrder from './components/AddOrder';
-import ProductDetail from './components/ProductDetail';
-
 const App = () => {
   return (
-   <>  
-  <Router>
-  <Routes>
-    <Route path="/register" element={<RegistrationForm />}  />
-    <Route path="/" element={<Navbar />} />
-  </Routes>
-  </Router>,
-  <HeroSecition />,
-  <CategorySection />,
-  <Card />,
-  <Testimonial />,
+    <>
+      <Router>
+        <Navbar>
+          <Link to="/register">Register</Link>
+        </Navbar>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <HeroSecition />
+              <CategorySection />
+              <Card />
+                <Testimonial />
   <AddOrder />
-   </>
+            </>
+          } />
+          <Route path="/register" element={<RegistrationForm />} />
+          <Route path="/" element={<Navbar />} />
+        </Routes>
+      </Router>
+    </>
   );
 };
 
